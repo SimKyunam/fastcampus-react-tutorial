2829c5394f5e4ec1ea2509d9c0f91a325c8aaad6
kyunam Sim <ppp9026@naver.com>
2020-12-28 09:04:37 +0900

    react props, 조건부 렌더링

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,27 +1,13 @@
 import Hello from './Hello';
 import './App.css';
+import Wrapper from './Wrapper';
 
 function App() {
-    // () 는 가독성을 위해서 사용하는 것, 안써도 소스는 돌아간다.
-    // 아래에 코드는 jsx, babel에서 제공해주는 것
-    const name = 'react';
-    //스타일은 객체로 넣어줘야한다.
-    const style = {
-        backgroundColor: 'black',
-        color: 'aqua',
-        fontSize: 24,
-        padding: '1rem',
-    };
     return (
-        //두개에 태그는 <>나 <div>로 꼭 감싸줘야한다!
-        <>
-            {/*주석 테스트*/}
-            <Hello
-            //이런식으로 작성하는 주석은 안나타남
-            />
-            <div style={style}>{name}</div>
-            <div className="gray-box"></div>
-        </>
+        <Wrapper>
+            <Hello name="react" color="red" isSpecial={true} />
+            <Hello color="pink" />
+        </Wrapper>
     );
 }
 
