68d2277a797cb8d8533271c5a7ae82abd04630b2
kyunam Sim <ppp9026@naver.com>
2021-01-01 23:37:57 +0900

    배열 추가 및 삭제 이벤트 작업

--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -1,18 +1,24 @@
 import React from 'react';
 
-function User({ user }) {
+function User({ user, onRemove }) {
+    const { username, email, id } = user;
     return (
         <div>
-            <b>{user.username}</b> <span>({user.email})</span>
+            <b>{username}</b> <span>({email})</span>
+            <button onClick={() => onRemove(id)}>삭제</button>
+            {/*
+                () => onRemove(id)가 아닌 onRemove(id)만 있을 경우 
+                시작과 동시에 호출 목록 안나옴, 함수를 생성해야함
+            */}
         </div>
     );
 }
 
-function UserList({ users }) {
+function UserList({ users, onRemove }) {
     return (
         <div>
-            {users.map(user => (
-                <User user={user} key={user.id} />
+            {users.map((user, index) => (
+                <User user={user} key={user.id} onRemove={onRemove} />
             ))}
         </div>
     );
